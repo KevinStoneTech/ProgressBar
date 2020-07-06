3cfcf69fd7a0200d484449bb9c4ff6debbfea328
Kevin Stone <kevin.stone.silva@gmail.com>
2020-07-06 19:59:32 -0400

    ProgressBar updating page

--- progressBar.js
+++ progressBar.js
@@ -4,27 +4,29 @@ const pageHome = document.querySelector("#page-create-point")
 // Criar a barrinha de progresso
 let bar = document.createElement("div")
 // Criar os estilos da barrinha
-bar.style.height = "4px"
+bar.style.height = "5px"
 bar.style.width = "0"
-bar.style.backgroundColor = "#6633cc"
+bar.style.backgroundColor = "#34cb79"
 bar.style.position = "fixed"
 bar.style.top = "0"
 bar.style.left = "0"
 bar.style.zIndex = "9999"
-bar.style.transition = "0.2s"
-
+bar.style.transition = "0.3s"
 // adiciona no corpo da pádina
 document.body.append(bar)
 
-// verifica o movimento do scroll
-document.addEventListener("scroll", function() {
+function updateBar(){
   // o tamanho da caixa que contem o texto
-  // console.log(pageHome.offsetHeight)
   const textHeight = pageHome.offsetHeight
   // verifica em que posiçao da pagina eu estou
   const pagePositionY = window.pageYOffset
   // regra de 3
-  const updatedBar =  pagePositionY * 150 / textHeight
+  const updatedBar =  pagePositionY * 180 / textHeight
 
   bar.style.width = updatedBar + "%"
+}
+
+window.addEventListener("load", () => {
+    // verifica o movimento do scroll
+    document.addEventListener("scroll", updateBar)
 })
